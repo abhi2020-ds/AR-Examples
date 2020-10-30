9ebac456c9439c66f2450ca3f46821733e54c7dc
abhi2020-ds <61113945+abhi2020-ds@users.noreply.github.com>
2020-10-30 18:58:48 -0400

    Add AR Examples to Master

---
+++ ARDemo/Assets/Scripts/BackandClose.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackandClose : MonoBehaviour
+{
+
+public void backbtn()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+public void exitbtn()
+    {
+        Application.Quit();
+        Debug.Log("Exit Button Is Pressed");
+    }
+
+}
---
+++ ARDemo/Assets/Scripts/ChangeSceneMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneMenu : MonoBehaviour
+{
+    public void car()
+    {
+        SceneManager.LoadScene("CarTest");
+    }
+
+    public void exitgame()
+    {
+        Application.Quit();
+        Debug.Log("Exit Button Pressed");
+    }
+}
